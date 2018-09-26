8fa026aed8f5614a3754e6768f3661bda9ec90d9
Yoshika <Yoshikakeating@gmail.com>
2018-09-26 23:31:01 +0000

    Adding last practice

---
+++ ingredients.js
@@ -0,0 +1,20 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda",
+"baking powder", "chocolate chips", "bananas"];
+
+
+var i = 0;
+while(i < 1){
+  console.log(ingredients);
+  i++;
+}
+
+
+ for(let i = 0; i < ingredients.length; i++){
+  console.log(ingredients[i]);
+ };
+
+
+
+ for(let i = 7; i >= 0; i--){
+  console.log(ingredients[i]);
+ };
